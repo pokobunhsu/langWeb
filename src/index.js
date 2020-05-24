e7532cb3f393b9a953008c9faa832853c91ca4a1
PokoHsu <minshanzen@gmail.com>
2020-05-25 06:34:57 +0800

    [Add]新增紅包偵測功能

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import CryptoJS from 'crypto-js';
 import Base64 from 'crypto-js/enc-base64';
 import HmacSHA256 from 'crypto-js/hmac-sha256';
 
-
 const server = CROS_SERVER;
 var token
 var uid
@@ -15,8 +14,10 @@ var liver_uid
 var devid
 var tk = "";
 var un = "";
+var canopen = "n";
 
 let socket
+var romsocket
 
 function init() {
     let xhr = new XMLHttpRequest();
@@ -149,11 +150,12 @@ function sendmsg() {//傳送使用者所輸入訊息
     document.getElementById("chat").innerHTML += '<div class="msg-box-msg my-1 px-2 py-1 rounded-pill" >' + "你" + "：" + message + '</div>';
     document.getElementById("msg").value = "";
     document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
-    ocket.send('42/chat_nsp,["msg",{"name":"' + un + '","grade_id":1,"grade_lvl":5,"lv":3,"lang_fans":"0","award_icon":"","medal":"","msg":"' + message + '","p_ic":"","g_lvl":"0","rel_color_lvl":1,"r_ic":"","n_cr":"#0c08ff","rel_color":"#0c08ff"}]');
-    //socket.send('42/chat_nsp,["msg",{"pfid":"3861468","live_id":"3652550Y104122Rev","is_admin":false,"lv":25,"name":"poko🍕格格真的飽了","msg":"龍騎士表示開心XD","medal":"","type":0,"rel_color":"#DFAF5E","n_cr":"#F8BBD0","vip_fan":2,"grade_id":1,"grade_lvl":87,"ugid":1,"uglv":78,"g_lvl":"0","r_ic":"","p_ic":"","lf_type":5,"a_ic":"","is_easter_egg":0,"i_sb_rid":0,"a_sb_rid":0,"at":1589814679994}]');
+    socket.send('42/chat_nsp,["msg",{"name":"' + un + '","grade_id":1,"grade_lvl":5,"lv":3,"lang_fans":"0","award_icon":"","medal":"","msg":"' + message + '","p_ic":"","g_lvl":"0","rel_color_lvl":1,"r_ic":"","n_cr":"#0c08ff","rel_color":"#0c08ff"}]');
+    //socket.send('42/chat_nsp,["msg",{"pfid":"3861468","live_id":"3652550Y104122Rev","is_admin":false,"lv":25,"name":"poko🍕格格真的飽了","msg":":)","medal":"","type":0,"rel_color":"#DFAF5E","n_cr":"#F8BBD0","vip_fan":2,"grade_id":1,"grade_lvl":87,"ugid":1,"uglv":78,"g_lvl":"0","r_ic":"","p_ic":"","lf_type":5,"a_ic":"","is_easter_egg":0,"i_sb_rid":0,"a_sb_rid":0,"at":1589814679994}]');
 }
 function refresh() {//refresh避免直播間聊天斷線
     socket.send('2');
+    romsocket.send('2');
 }
 function flv_start() {//點擊進入直播間後順便開始撥放串流、將按鈕停用並顯示已連線--flv用
     document.getElementById("videoElement").play();
@@ -210,16 +212,84 @@ function connect() {
     getvideo()
     flv_start()
     hotWord()
+    room()
 }
+function room() {
+    var tk = $("#myat").val();
+    romsocket = new WebSocket('wss://ctl-q3.lv-show.com/socket.io/?EIO=3&transport=websocket');
+    romsocket.addEventListener('open', function (event) {
+        romsocket.send('40/control_nsp');
+    });
+    romsocket.addEventListener('open', function (event) {
+        romsocket.send('42/control_nsp,["authentication",{"live_id":"' + live_id + '","anchor_pfid":"' + liver_uid + '","access_token":"' + tk + '","token":"' + tk + '","from":"WEB","client_type":"web","r":0}]');
+    });
+    romsocket.addEventListener('message', function (event) {
+        let msg = event.data;
+        msg = JSON.parse(msg.replace("42/control_nsp,", ""));
+        try {
+            if (msg[1].data.Event == "packet_delay_list_ex") {
+                let dalaytime = msg[1].data.list[0].delay;
+                document.getElementById("red_time").innerText = dalaytime + "秒";
+                console.log("等等有紅包，剩餘時間:" + dalaytime + "秒");
+            }
+            if(msg[1].data.Event == "sunpacket_create") {
+                let stamp = msg[1].data.stamp;
+                let sun = 1;//1陽光，0浪花
+                let xhr = new XMLHttpRequest();
+                xhr.open('POST', server + 'https://langapi.lv-show.com/v2/redpacket/open');
+                xhr.setRequestHeader('PLATFORM', 'WEB');
+                xhr.setRequestHeader('LOCALE', 'TW');
+                xhr.setRequestHeader('USER-TOKEN', token);
+                xhr.setRequestHeader('VERSION', '5.0.0.7');
+                xhr.setRequestHeader('API-VERSION', '2.0');
+                xhr.setRequestHeader('USER-UID', uid);
+                xhr.setRequestHeader('DEVICE-ID', devid);
+                xhr.setRequestHeader('USER-MPHONE-OS-VER', '9');
+                xhr.setRequestHeader('VERSION-CODE', '1280');
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xhr.send(`live_id=${live_id}&f_pfid=0&stamp=${stamp}&is_sun=${sun}`);
+                xhr.addEventListener("load", function (evt) {
+                    var JDATA = JSON.parse(xhr.responseText);
+                    document.body.innerHTML+=`<div class="alert alert-warning alert-dismissible" role="alert" style="position: fixed;bottom: 0;right:0;z-index: 1;">恭喜你搶到了<span id="sunget">${JDATA.data.gold}</span>陽光!!</div>`;
+                    $('.alert').fadeOut(5000);
+                    $('.alert').alert('close');
+                    console.log("搶到了"+JDATA.data.gold+"陽光!");
+                });
+            }else{} 
+        } catch (error) {
+            
+        } 
+    });
+}
+// function redopen(live_id, stamp, sun) {
+//     if (canopen == "y") {
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('POST', 'https://langapi.lv-show.com/v2/redpacket/open');
+//         xhr.setRequestHeader('PLATFORM', 'WEB');
+//         xhr.setRequestHeader('LOCALE', 'TW');
+//         xhr.setRequestHeader('USER-TOKEN', token);
+//         xhr.setRequestHeader('VERSION', '5.0.0.7');
+//         xhr.setRequestHeader('API-VERSION', '2.0');
+//         xhr.setRequestHeader('USER-UID', uid);
+//         xhr.setRequestHeader('DEVICE-ID', devid);
+//         xhr.setRequestHeader('USER-MPHONE-OS-VER', '9');
+//         xhr.setRequestHeader('VERSION-CODE', '1280');
+//         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//         xhr.send(`live_id=${live_id}&f_pfid=0&stamp=${stamp}&is_sun=${sun}`);
+//         xhr.addEventListener("load", function (evt) {
+//             var JDATA = JSON.parse(xhr.responseText);
+//             console.log(JDATA.data);
+//         });
+//     }
+// }
 
-//
+
 window.onload = function () {
     // console.log("window loaded")
-
     token = getParams("token");//接收來html上的value
     uid = getParams("userid");//接收來html上的value
     live_id = getParams("live_id"); //請更改主播live ID
-    liver_uid = getParams("live_uid"); //請更改主播UID
+    liver_uid = getParams("live_uid"); //0524已改為從上方funtion取得，但註解會造成錯誤~請勿註解
     devid = Math.random().toString(36).substr(2, 678) + Date.now().toString(36).substr(4, 585);
 
 
@@ -227,5 +297,4 @@ window.onload = function () {
     document.getElementById("message_send").onclick = sendmsg
 
     init()
-
 };
